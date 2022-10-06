eb58211308f3bd97e923e07ddeb7b3623898bd99
Filcar <imfilcarpt@gmai.com>
2022-10-06 14:33:37 +0100

    add JUnit Tests Class

--- src/test/java/JUnitTests.java
+++ src/test/java/JUnitTests.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class JUnitTests {
+import jpass.crypt.DecryptException;
+import jpass.util.Configuration;
+import jpass.util.DateUtils;
+import jpass.util.StringUtils;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.format.DateTimeFormatter;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class JUnitTests {
+    
+
 }
