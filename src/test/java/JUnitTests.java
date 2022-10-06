87c48b668ca5933e6bdb9ab1ea5148ea587f00fd
Filcar <imfilcarpt@gmai.com>
2022-10-06 17:08:46 +0100

    2º function test: SetClipboardContent

--- src/test/java/JUnitTests.java
+++ src/test/java/JUnitTests.java
@@ -1,10 +1,13 @@
 import jpass.crypt.DecryptException;
+import jpass.util.ClipboardUtils;
 import jpass.util.Configuration;
 import jpass.util.DateUtils;
 import jpass.util.StringUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -23,6 +26,15 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class JUnitTests {
 
+    @BeforeAll
+    static void initAll() {
+    }
+
+    @BeforeEach
+    void init() {
+    }
+
+    //1º Function
     @ParameterizedTest
     @MethodSource("StringStripValues")
     public void TestStripString(String expected, String value, Integer lenght) throws IOException {
@@ -36,6 +48,51 @@ public class JUnitTests {
                 arguments("abcde...", "abcdefg", 5),
                 arguments("abcdefg", "abcdefg", 7)
         );
+    }
+
+    //2º Function
+    @ParameterizedTest
+    @MethodSource("StringClipboard")
+    public void TestSetClipboardContent(String value) throws IOException {
+
+        try {
+            ClipboardUtils.setClipboardContent(value);
+            String cliboardContent = ClipboardUtils.getClipboardContent();
+            System.out.println(cliboardContent);
+            assertEquals(value, cliboardContent);
+            ClipboardUtils.clearClipboardContent();
+        }
+        catch(Exception e) {
+            System.out.println(e);
+            //  Block of code to handle errors
+        }
+    }
+    private static Stream<Arguments> StringClipboard() {
+        return Stream.of(
+                arguments("valueRandomOne"),
+                arguments("valueRandomSecond"),
+                arguments("valueRandomThird")
+        );
+    }
+
+    //3º Function
+
+
+    //4º Function
+
+
+
+
+    //5º Function
+
+
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @AfterAll
+    static void tearDownAll() {
     }
 
 }
